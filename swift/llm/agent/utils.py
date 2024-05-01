e063282843b3adc6b721d7e43506aaac74db5f55
yuze.zyz <yuze.zyz@alibaba-inc.com>
2024-05-01 21:11:01 +0800

    add test code

--- swift/llm/agent/utils.py
+++ swift/llm/agent/utils.py
@@ -1,4 +1,5 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+import os
 from typing import List, Tuple
 
 from swift.utils import get_logger
@@ -32,8 +33,31 @@ def calculate_loss_scale(response: str, use_loss_scale=False) -> Tuple[List[str]
     Returns:
         A tuple of agent response parts and their weights.
     """
+    if 'outline' in response and os.environ.get('outline', None) == '1':
+        parts = split_str_parts_by(response, ['outline:', 'answer:',
+                                              'Thought finished', 'thought finished', 'Thought Finished'])
+        weights = []
+        contents = []
+        for c in parts:
+            if c['key'] == 'outline:':
+                if c['content'].strip():
+                    weights += [1.0]
+                    contents.append(c['content'])
+            elif c['key'].lower() == 'thought finished':
+                weights += [1.0]
+                weights += [0.0]
+                if c['content'].startswith('.'):
+                    c['content'] = c['content'][1:]
+                contents.append(c['key'] + '.')
+                contents.append(c['content'])
+            else:
+                weights += [0.0]
+                contents.append(c['content'])
+        return contents, weights
     if 'Action:' in response and 'Observation:' in response and use_loss_scale:
-        agent_keyword = ['Action:', 'Action Input:', 'Thought:', 'Final Answer:', 'Observation:']
+        agent_keyword = ['Action:', 'Action Input:', 'Thought:',
+                         'Final Answer:', 'Observation:', 'outline:',
+                         'Thought finished', 'thought finished', 'Thought Finished']
         agent_parts = split_str_parts_by(response, agent_keyword)
         weights = []
         agent_content = []
@@ -41,14 +65,29 @@ def calculate_loss_scale(response: str, use_loss_scale=False) -> Tuple[List[str]
             if c['key'] in ('Action:', 'Action Input:'):
                 weights += [2.0]
                 weights += [2.0]
+                agent_content.append(c['key'])
+                agent_content.append(c['content'])
             elif c['key'] in ('Thought:', 'Final Answer:', ''):
                 weights += [1.0]
                 weights += [1.0]
+                agent_content.append(c['key'])
+                agent_content.append(c['content'])
             elif c['key'] in ('Observation:', ):
                 weights += [2.0]
                 weights += [0.0]
-            agent_content.append(c['key'])
-            agent_content.append(c['content'])
+                agent_content.append(c['key'])
+                agent_content.append(c['content'])
+            elif c['key'] == 'outline:':
+                if c['content'].strip():
+                    weights += [0.0]
+                    agent_content.append(c['content'])
+            elif c['key'].lower() == 'thought finished':
+                weights += [0.0]
+                weights += [1.0]
+                if c['content'].startswith('.'):
+                    c['content'] = c['content'][1:]
+                agent_content.append(c['key'] + '.')
+                agent_content.append(c['content'])
         return agent_content, weights
     elif ('Action:' in response or 'Next:' in response) and use_loss_scale:  # alpha-umi
         agent_keyword = ['Next:', 'Action:', 'Action Input:']
