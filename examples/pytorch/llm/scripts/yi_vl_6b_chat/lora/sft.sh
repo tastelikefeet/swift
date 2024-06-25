7ac082219d7dc4fe53124661fcb4de59581c400e
tastelikefeet <yuze.zyz@alibaba-inc.com>
2024-06-25 22:20:48 +0800

    fix

--- examples/pytorch/llm/scripts/yi_vl_6b_chat/lora/sft.sh
+++ examples/pytorch/llm/scripts/yi_vl_6b_chat/lora/sft.sh
@@ -9,18 +9,18 @@ swift sft \
     --output_dir output \
     --dataset llava-pretrain \
     --train_dataset_sample -1 \
-    --num_train_epochs 1 \
+    --num_train_epochs 2 \
     --max_length 4096 \
     --check_dataset_strategy warning \
     --gradient_checkpointing true \
     --batch_size 2 \
     --weight_decay 0.1 \
     --learning_rate 5e-5 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps 64 \
     --max_grad_norm 0.5 \
     --warmup_ratio 0.03 \
-    --eval_steps 1000 \
-    --save_steps 1000 \
+    --eval_steps 200 \
+    --save_steps 200 \
     --save_total_limit 4 \
     --logging_steps 10 \
     --use_flash_attn true \
